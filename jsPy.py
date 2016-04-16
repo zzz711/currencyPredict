5f068480328647ec6b3ede0ce24f45260522b5d0
zzz711 <zzz8910@gmail.com>
2016-04-16 19:25:52 -0400

    getting exchange rates from 2000 to now

--- jsPy.py
+++ jsPy.py
@@ -1,9 +1,36 @@
 import urllib.request
 import json
 import codecs
+import datetime
 
-req = urllib.request.urlopen("http://api.fixer.io/2000-01-03").read().decode('utf8')
+now = datetime.datetime.now()
+
+data = [] 
+count = 0
+#url = "http://api.fixer.io/"
+
+for year in range(2000,  now.year + 1):
+	for month in range(1, 13):
+		url = "http://api.fixer.io/"
+		
+		mnth = month
+		if(month < 10):
+			mnth = "0" + str(month)
+		else:
+			mnth = str(month)
+			
+		url = url  + str(year) + "-" + mnth + "-15"
+		
+		if(year == now.year and month > now.month):
+			break 
+		
+		req = urllib.request.urlopen(url).read().decode('utf8')
+		sn = json.loads(req)
+		data.insert(count, sn)
+		count += 1
+
+#req = urllib.request.urlopen("http://api.fixer.io/2000-01-03").read().decode('utf8')
 #reader = codecs.getreader("utf-8")
-jsn = json.loads(req)
-print(jsn)
+for jsn in data:
+	print(jsn["rates"]["USD"])
 
