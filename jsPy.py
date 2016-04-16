a2cebb407f0595191829b9442992703fa2490f4f
zzz711 <zzz8910@gmail.com>
2016-04-16 16:54:11 -0400

    converted data from http request to JSON

--- jsPy.py
+++ jsPy.py
@@ -1,5 +1,9 @@
 import urllib.request
 import json
+import codecs
 
-req = urllib.request.urlopen("http://api.fixer.io/2000-01-03").read()
+req = urllib.request.urlopen("http://api.fixer.io/2000-01-03").read().decode('utf8')
+#reader = codecs.getreader("utf-8")
+jsn = json.loads(req)
+print(jsn)
 
