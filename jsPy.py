8543e38377546a43df3c7d093cb007e52c88e590
Adam White <adam.nathaniel.white@gmail.com>
2016-04-16 21:18:27 -0400

    Add Australia rate, and fix bug in China rate.

--- jsPy.py
+++ jsPy.py
@@ -36,15 +36,20 @@ for year in range(2000,  now.year + 1):
 
 
 # GET THE USD FEDERAL FUNDS RATE
-print("USD FEDERAL FUNDS RATE:")
+#print("USD FEDERAL FUNDS RATE:")
 federalFundsReq = urllib.request.urlopen("https://www.quandl.com/api/v3/datasets/FRED/FEDFUNDS.json").read().decode('utf8')
 federalFunds = json.loads(federalFundsReq)
 #for month in federalFunds['dataset']['data'] :
 #        print(month)
 
 # GET THE RMB CHINA PBOC RATE
-print("RMB PBOC RATE:")
-pcobReq = urllib.request.urlopen("https://www.quandl.com/api/v3/datasets/FRED/FEDFUNDS.json").read().decode('utf8')
-pcobRate = json.loads(pcobReq)
-for month in pcobRate['dataset']['data'] :
-        print(month)
+#print("RMB PBOC RATE:")
+chinaReq = urllib.request.urlopen("https://www.quandl.com/api/v3/datasets/BCB/17899.json").read().decode('utf8')
+chinaRate = json.loads(pcobReq)
+#for month in pcobRate['dataset']['data'] :
+#        print(month)
+
+
+# GET THE AUSTRALIA CENTRAL BANK RATE
+australiaReq = urllib.request.urlopen("https://www.quandl.com/api/v3/datasets/SGE/AUSIR.json?api_key=iww131CxHzH6-BQL_adz&start_date=1970-01-01&end_date=1970-01-01").read().decode('utf8')
+australiaRate = json.loads(pcobReq)
